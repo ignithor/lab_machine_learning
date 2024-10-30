214a1ec30826bcc26114bac1c281a781a786caeb
Paul P <paul.phamdang@gmail.com>
2024-10-30 17:27:28 +0100

    Add Lab2

---
+++ Lab2/lab2_linear_regression.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+ 
+
+
+
+def main():
+    # Load the data
+    dataset = pd.read_csv('turkish-se-SP500vsMSCI.csv')
+    dataset = pd.read_csv('mtcarsdata-4features.csv')
+    
+
+    pass
